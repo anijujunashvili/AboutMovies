51a9401f84ed3a256071f7c10aa14f6d78d0bb31
Ani Jujunashvili <ana.jujunashvili@gmail.com>
2025-01-26 16:08:48 +0400

    search dark mode

--- src/components/header/right-section/search/index.tsx
+++ src/components/header/right-section/search/index.tsx
@@ -11,24 +11,30 @@ import dayjs from "dayjs";
 import { APP_PATHS } from "@/routes/enum";
 import { searchType } from "@/types/search";
 import { useTranslation } from "react-i18next";
-
-const customStyles = {
-  content: {
-    top: "40%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    width: "50%",
-    minHeight: "400px",
-    marginRight: "-50%",
-    backfround: "red",
-    transform: "translate(-50%, -50%)",
-  },
-};
+import { useTheme } from "@/components/theme-provider";
 
 Modal.setAppElement("#root");
 
 const SearchComp = () => {
+  const { theme } = useTheme();
+  const customStyles = {
+    overlay: {
+      background: "rgba(0,0,0,0.7)",
+    },
+    content: {
+      top: "40%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      backgroundColor: theme === "dark" ? "#020817" : "#fff",
+      width: "50%",
+      border: theme === "dark" ? "1px solid #374151" : "#fff",
+      minHeight: "400px",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
   const navigate = useNavigate();
   const [modalIsOpen, setIsOpen] = useState(false);
   const { lang } = useParams();
@@ -66,10 +72,13 @@ const SearchComp = () => {
           onRequestClose={closeModal}
           style={customStyles}
           contentLabel="Search"
-          portalClassName="bg-primary"
+          portalClassName=" bg-primary"
         >
-          <X onClick={closeModal} className="absolute right-4 cursor-pointer" />
-          <div className="flex flex-col justify-between">
+          <X
+            onClick={closeModal}
+            className="dark:text-secondary absolute right-4 cursor-pointer"
+          />
+          <div className="bg-background flex flex-col justify-between">
             <div className="relative flex w-[95%] items-center">
               <Controller
                 name="search"
@@ -80,29 +89,29 @@ const SearchComp = () => {
                       onChange={onChange}
                       value={value}
                       placeholder={t("layout.search")}
-                      className="focus:border-primary text-smoutline-none w-full rounded-none border-b bg-transparent py-3 pl-8 pr-8 shadow-none focus-visible:ring-0"
+                      className="focus:border-primary dark:text-secondary text-smoutline-none w-full rounded-none border-b bg-transparent py-3 pl-8 pr-8 shadow-none focus-visible:ring-0"
                     />
                   );
                 }}
               />
-              <Search className="absolute left-0 h-[18px] w-[18px] cursor-pointer" />
+              <Search className="dark:text-secondary absolute left-0 h-[18px] w-[18px] cursor-pointer" />
             </div>
 
             <div className="mt-6 flex flex-row flex-wrap gap-6">
               {searchResult?.map((res) => {
                 return (
                   <div
-                    className="flex max-h-[70px] cursor-pointer flex-row gap-2 border-b pb-2 hover:shadow-sm md:w-full lg:w-[45%]"
+                    className="flex max-h-[70px] cursor-pointer flex-row gap-2 border-b pb-2 hover:shadow-sm md:w-full lg:w-[45%] dark:border-gray-700"
                     key={res.id}
                   >
                     <img
                       src={
                         import.meta.env.VITE_SUPABASE_STORAGE_URL + res?.image
                       }
-                      className="h-full w-auto rounded-md"
+                      className="h-full w-auto rounded-md dark:border-gray-400"
                     />
                     <div className="flex flex-col space-y-1">
-                      <div className="mt-2 flex text-sm font-semibold hover:underline">
+                      <div className="dark:text-secondary mt-2 flex text-sm font-semibold hover:underline">
                         <Link to={APP_PATHS.MOVIES + "/" + res.id}>
                           {lang == "ka"
                             ? shortenText(res.name_ka, 50)
@@ -118,7 +127,7 @@ const SearchComp = () => {
               })}
             </div>
             <div
-              className="absolute bottom-4 cursor-pointer text-gray-500 hover:underline"
+              className="dark:text-secondary absolute bottom-4 cursor-pointer text-gray-500 hover:underline"
               onClick={toAdvancedSearch}
             >
               {t("layout.advanced_search")}
